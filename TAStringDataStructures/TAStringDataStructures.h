75bfe0b1196cae0da52350f98c6fd0899e5490bf
todd100state <todd@100state.com>
2014-09-29 11:16:45 -0700

    Initial Commit

---
+++ TAStringDataStructures/TAStringDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  TAStringDataStructures.h
+//  TAStringDataStructures
+//
+//  Created by Todd Anderson on 9/29/14.
+//  Copyright (c) 2014 ToddAnderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TAStringDataStructures.
+FOUNDATION_EXPORT double TAStringDataStructuresVersionNumber;
+
+//! Project version string for TAStringDataStructures.
+FOUNDATION_EXPORT const unsigned char TAStringDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TAStringDataStructures/PublicHeader.h>
+
+
